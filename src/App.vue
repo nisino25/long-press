772ac84299ceaa2d77d3b29694d6dbbd39ee50f8
nisino25 <nisino25@gmail.com>
2021-10-19 16:53:59 +0900

    first try

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<body   :style="[touching? {backgroundColor: 'BlueViolet'} : {backgroundColor: 'black'}]" @touchstart="touching = true;" @touchend="touching = false">
+  <h5 style="color:white; font-size: 200%">Welcome</h5>
+
+</body>
+
+ 
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      // touching: true,
+      touching: false,
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h5{
+  padding: 100px 100px;
+}
+</style>
